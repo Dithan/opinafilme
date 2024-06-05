259566a0f4b13c6544bc12f035d3a60b3c26b7c8
Alexandre Silva <alexandresargitario@gmail.com>
2024-06-05 15:22:47 -0300

    Card filme pesquisa integrado com api

--- src/app/componentes/card-filme-pesquisa/card-filme-pesquisa.component.ts
+++ src/app/componentes/card-filme-pesquisa/card-filme-pesquisa.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-card-filme-pesquisa',
@@ -6,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-filme-pesquisa.component.scss'],
 })
 export class CardFilmePesquisaComponent  implements OnInit {
+  constructor(public navCtrl: NavController, private apiService: ApiService) {}
 
-  constructor() { }
+  nomeFilme :string;
+  posterFilme :string;
+  idFilme :string
+  bioFilme :string
+  ngOnInit(): void {
+    this.apiService.getDadosDaAPI().subscribe((data) => {
+      this.idFilme = data.id;
+      this.bioFilme = data.descriptions[1].description;
 
-  ngOnInit() {}
+      this.nomeFilme = data.name;
+
+      this.posterFilme = data.images[0].href;
+    });
+  }
+
+  /* NAVEGA PARA: Filme */
+  navToFilme(itemId: string) {
+    this.navCtrl.navigateForward(`/tabs/filmes/filme/${itemId}`);
+  }
 
 }
