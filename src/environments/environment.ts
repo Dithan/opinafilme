ea80bec5672ad2c5c81c2fa8558f79cf0de3d66f
Alexandre Silva <alexandresargitario@gmail.com>
2024-06-06 15:28:16 -0300

    corrigindo problema environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,7 @@
 
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyD_Q2Qe5SNumcdvGb-lfkRXaX0jWphSyWU",
-    authDomain: "speedy-anthem-398213.firebaseapp.com",
-    databaseURL: "https://speedy-anthem-398213-default-rtdb.firebaseio.com",
-    projectId: "speedy-anthem-398213",
-    storageBucket: "speedy-anthem-398213.appspot.com",
-    messagingSenderId: "12411617863",
-    appId: "1:12411617863:web:c0e6bb531ee1f3c838a762",
-    measurementId: "G-56YG3XZ6C0"
-}
+
 }
 /*
  * For easier debugging in development mode, you can import the following file
