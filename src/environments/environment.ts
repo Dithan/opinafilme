6d3a09fbb5255ee06d648fec116b23d309321c02
Alexandre Silva <alexandresargitario@gmail.com>
2024-06-10 12:04:47 -0300

    errei

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,14 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyD_Q2Qe5SNumcdvGb-lfkRXaX0jWphSyWU',
-    authDomain: 'speedy-anthem-398213.firebaseapp.com',
-    databaseURL: 'https://speedy-anthem-398213-default-rtdb.firebaseio.com',
-    projectId: 'speedy-anthem-398213',
-    storageBucket: 'speedy-anthem-398213.appspot.com',
-    messagingSenderId: '12411617863',
-    appId: '1:12411617863:web:c0e6bb531ee1f3c838a762',
-    measurementId: 'G-56YG3XZ6C0',
-  },
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"
+  }
 };
 /*
  * For easier debugging in development mode, you can import the following file
