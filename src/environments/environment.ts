f8a119faa8455760dd0f87ac6515666add4a2041
Alexandre Silva <alexandresargitario@gmail.com>
2024-06-14 15:25:11 -0300

    environment atualizado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,16 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
-    measurementId: "YOUR_MEASUREMENT_ID"
-  }
+    apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+  },
+  apikey:""
 };
 /*
  * For easier debugging in development mode, you can import the following file
