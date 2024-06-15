a56fa99ac8441e2d487cc7c2eb318496a56e352c
Thiago Rocha <thiagodesousarocha1311@gmail.com>
2024-06-15 14:53:04 -0300

    STYLE: Ajustes de CSS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,14 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: '',
+    apiKey: 'AIzaSyD_Q2Qe5SNumcdvGb-lfkRXaX0jWphSyWU',
+    authDomain: 'speedy-anthem-398213.firebaseapp.com',
+    databaseURL: 'https://speedy-anthem-398213-default-rtdb.firebaseio.com',
+    projectId: 'speedy-anthem-398213',
+    storageBucket: 'speedy-anthem-398213.appspot.com',
+    messagingSenderId: '12411617863',
+    appId: '1:12411617863:web:c0e6bb531ee1f3c838a762',
+    measurementId: 'G-56YG3XZ6C0',
   },
   apikey: '6ac68465&i',
 };
-
